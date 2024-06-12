2acf91fab94b7efbf45c081d0fdd3c87ab4e599f
Yusuf Morsi <ymorsi7@gmail.com>
2024-06-11 21:47:20 -0700

    Update train.py

--- train.py
+++ train.py
@@ -5,82 +5,69 @@ import matplotlib.pyplot as plt
 import torchvision.utils as vutils
 import numpy as np
 
-def trainDiscriminator(D, G, optimD, data, batch_size, device):
-
-    '''
-    Trains the Discriminator
-    Inputs: Discriminator, Generator, Discriminator's optimizer, trainLoader, 128 batch size, device
-    Output: loss
-    '''
-    criterion = nn.BCELoss()
-    D.zero_grad()
-    realOutput = D(data.to(device))
-    realLoss = criterion(realOutput, torch.ones_like(realOutput).to(device))
+def trainDiscriminator(discriminator, generator, optimizerD, realData, batchSize, device):
+    lossFunction = nn.BCELoss()
+    optimizerD.zero_grad()
+    realData = realData.to(device)
+    realLabels = torch.ones(realData.size(0), 1, device=device)
+    realOutput = discriminator(realData)
+    realLoss = lossFunction(realOutput, realLabels)
     realLoss.backward()
 
-    noise = torch.randn(batch_size, 100, 1, 1).to(device)
-    fakeOutput = D(G(noise).detach())
-    fakeLoss = criterion(fakeOutput,  torch.zeros_like(fakeOutput, dtype = torch.float).to(device))
+    noise = torch.randn(batchSize, 100, 1, 1, device=device)
+    fakeData = generator(noise).detach()
+    fakeLabels = torch.zeros(fakeData.size(0), 1, device=device)
+    fakeOutput = discriminator(fakeData)
+    fakeLoss = lossFunction(fakeOutput, fakeLabels)
 
     fakeLoss.backward()
-    optimD.step()
-    loss = fakeLoss + realLoss
-    return loss
+    optimizerD.step()
+    
+    totalLoss = realLoss + fakeLoss
+    return totalLoss
 
-def trainGenerator(D, G, optimG, data, batch_size, device):
-    '''
-    Trains the Generator
-    Inputs: Discriminator, Generator, Generator's optimizer, trainLoader, 128 batch size, device
-    Output: loss
-    '''
-    criterion = nn.BCELoss()
-    G.zero_grad()
-    noise = torch.randn(batch_size, 100, 1, 1).to(device)
-    output = D(G(noise))
-    loss = criterion(output, torch.ones_like(output))
+    return totalLoss
+
+def trainGenerator(discriminator, generator, optimizerG, realData, batchSize, device):
+    lossFunction = nn.BCELoss()
+    generator.zero_grad()
+    noise = torch.randn(batchSize, 100, 1, 1).to(device)
+    output = discriminator(generator(noise))
+    loss = lossFunction(output, torch.ones_like(output))
     loss.backward()
 
-    optimG.step()
+    optimizerG.step()
     return loss
 
-def trainProcess(epoch_num, train_loader, D, G, optimD, optimG, dLoss, gLoss, lr, kernSize, padVal, device):
-    '''
-    Trains the models on epoch_num epochs, saving models and printing results
-    Inputs: epoch_num, train loader, discriminator, generator, both optimizers, 
-    both loss, learning rate, kernel size, padding, device
-    Outputs: loss over iterations
-    '''
-    for epoch in range(epoch_num):
-        for i, (images, _) in enumerate(train_loader):
-            dError  = trainDiscriminator(D, G, optimD, images, 128, device)
-            gError = trainGenerator(D, G, optimG, images, 128, device)
-        dLoss.append(dError.cpu().tolist())
-        gLoss.append(gError.cpu().tolist())
+def trainProcess(numEpochs, dataLoader, discriminator, generator, optimizerD, optimizerG, discLosses, genLosses, learningRate, kernelSize, padding, device):
+    epochCounter = 0
+    while epochCounter < numEpochs:
+        for batchIndex, (realImages, _) in enumerate(dataLoader):
+            discLossValue = trainDiscriminator(discriminator, generator, optimizerD, realImages, 128, device)
+            genLossValue = trainGenerator(discriminator, generator, optimizerG, realImages, 128, device)
+        discLosses.append(discLossValue.cpu().tolist())
+        genLosses.append(genLossValue.cpu().tolist())
+        epochCounter += 1
 
-    torch.save(G, f'G_{epoch_num}_{lr}_{kernSize}-{padVal}.pt')
-    torch.save(D, f'D_{epoch_num}_{lr}_{kernSize}-{padVal}.pt')
+    torch.save(generator, f'Gen_{numEpochs}_{learningRate}_{kernelSize}-{padding}.pt')
+    torch.save(discriminator, f'Disc_{numEpochs}_{learningRate}_{kernelSize}-{padding}.pt')
+    print(f'After {numEpochs} epochs with learning rate = {learningRate}, kernel size = {kernelSize}, padding = {padding}: \tDiscriminator Loss: {discLossValue}\t Generator Loss: {genLossValue}\n')
 
-    print(f'After {epoch_num} epochs with lr = {lr}, kernel size = {kernSize}, padding = {padVal}: \tDiscriminator Loss: {dError}\t Generator Loss: {gError}\n')
+    return discLosses, genLosses
 
-    return dLoss, gLoss
-
-def trainResults(epoch_num, G, dLoss, gLoss, device):  
-    '''
-    Plots the results from the training process
-    Inputs: epoch_num, generator, loss over iterations, device
-    '''
-    fake_images = G(torch.randn(64, 100, 1, 1).to(device)).detach().cpu()
-    fake_images = vutils.make_grid(fake_images, padding = True)
+def trainResults(numEpochs, generator, discLosses, genLosses, device):  
+    fakeImages = generator(torch.randn(64, 100, 1, 1).to(device)).detach().cpu()
+    fakeImages = vutils.make_grid(fakeImages, padding=True)
     plt.figure()
-    plt.plot(dLoss, label = "Discriminator")
-    plt.plot(gLoss, label = "Generator")
+    plt.plot(discLosses, label="Discriminator")
+    plt.plot(genLosses, label="Generator")
     plt.xlabel("Number of Epochs")
     plt.ylabel("Loss")
     plt.legend()
     plt.title("Discriminator and Generator Loss Over Epochs")
 
-    plt.figure(figsize = (8, 8))
+    plt.figure(figsize=(8, 8))
     plt.axis("off")
-    plt.title(f"Fake Images After {epoch_num} Epochs")
-    plt.imshow(np.transpose(fake_images,(1,2,0)))
+    plt.title(f"Fake Images After {numEpochs} Epochs")
+    plt.imshow(np.transpose(fakeImages, (1, 2, 0)))
     plt.show()
